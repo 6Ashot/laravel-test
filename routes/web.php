e67a2e244be1c14dd69fd0cf9b912fc5d3d452eb
Ashot <khanoyan95@mail.ru>
2019-12-04 17:39:34 +0400

    fix auth route

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,15 @@ Route::get('/', function () {
 
 // Auth::routes();
 
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+// Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+// Route::post('login', 'Auth\LoginController@login');
+// Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('companies','CompaniesController');
 Route::resource('employees','EmployeesController');
 
-// Auth::routes();
+Auth::routes(['register' => false]);
 
 // Route::get('/home', 'HomeController@index')->name('home');
